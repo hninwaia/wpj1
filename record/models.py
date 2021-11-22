f5acbeed5961116fa7605a5e09fc1efb38278860
hninwaia <hninwaiwaiag323698@gmail.com>
2021-11-22 11:37:23 +0630

    first commit

---
+++ record/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Record(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    date = models.DateField()
+
+    def __ str __(self):
+        return self.title
